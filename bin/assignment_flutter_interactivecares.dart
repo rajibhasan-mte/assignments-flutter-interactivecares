3c0efcfb816ee00e48d9d92028a8a8ff56193c91
Rajib Hasan <rajibhasan.mte@mail.com>
2025-12-26 17:15:51 +0600

    update: project

---
+++ bin/assignment_flutter_interactivecares.dart
@@ -0,0 +1,5 @@
+import 'package:assignment_flutter_interactivecares/assignment_flutter_interactivecares.dart' as assignment_flutter_interactivecares;
+
+void main(List<String> arguments) {
+  print("Hello");
+}
---
+++ test/assignment_flutter_interactivecares_test.dart
@@ -0,0 +1,8 @@
+import 'package:assignment_flutter_interactivecares/assignment_flutter_interactivecares.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
