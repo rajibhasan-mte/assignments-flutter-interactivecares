305cd17b7f1e2a235c54fc4fb536d0c9cb3f3bb0
Rajib Hasan <rajibhasan.mte@mail.com>
2025-12-26 16:39:47 +0600

    added: intelij project

---
+++ example/assignments_flutter_interactivecares_example.dart
@@ -0,0 +1,6 @@
+import 'package:assignments_flutter_interactivecares/assignments_flutter_interactivecares.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
