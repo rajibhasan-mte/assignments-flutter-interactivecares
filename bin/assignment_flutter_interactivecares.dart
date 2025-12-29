b201164c93d6cc487a6ece160407a21f32a5ee34
Rajib Hasan <rajibhasan.mte@mail.com>
2025-12-29 13:12:57 +0600

    Final: -

--- bin/assignment_flutter_interactivecares.dart
+++ bin/assignment_flutter_interactivecares.dart
@@ -1,14 +1,25 @@
 import 'package:assignment_flutter_interactivecares/assignment_flutter_interactivecares.dart' as assignment_flutter_interactivecares;
 
+/// ************** Assignment-1: Working with Sets in Dart ****************
+/// Author: Rajib Hasan
+/// Date: 29 December, 2025
 
 
 
 
-
-
-
+Set<int> A = {1, 2, 3, 4, 5}, B = {4, 5, 6, 7, 8};
 
 
 void main(List<String> arguments) {
-  print("Hello");
+
+  print("Element of Set A:");
+  for(int x in A) {
+    print(x);
+  }
+
+  print("\nElement of Set B:");
+  for(int x in B){
+    print(x);
+  }
+
 }
