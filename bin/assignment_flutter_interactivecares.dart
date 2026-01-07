fc2adcaae85c8745686e77881d56f2df6074a4b2
Rajib Hasan <rajibhasan.mte@gmail.com>
2026-01-08 01:07:24 +0600

    success: ready to submission

--- bin/assignment_flutter_interactivecares.dart
+++ bin/assignment_flutter_interactivecares.dart
@@ -1,26 +1,44 @@
 import 'package:assignment_flutter_interactivecares/assignment_flutter_interactivecares.dart' as assignment_flutter_interactivecares;
 
-/// ************** Assignment-1: Working with Sets in Dart ****************
+/// --------------------------------------------------------------------------------------------
 /// Author: Rajib Hasan
-/// Submisson date: 29 December, 2025
-/// Submission to: Flutter course: Interactivecares.com
+/// Submisson date:  07.01.2026
+/// Submission to: interactivcecares flutter course
+/// submission links: https://interactivecares.com/dashboard/334/content?tab=live&module=4727&id=16
+///  ------------------- Assignment 2: BMI Calculator Using Dart Function -----------------------
+/// BMI Calculation Function (40 Marks)
+/// - BMI = weight / (height × height)
+/// Part B: BMI Category Function (40 Marks)
+/// Part C: Output Display (20 Marks)
+/// -Print BMI value (up to 2 decimal places)***
+/// Sample Output-
+/// Your BMI is: 23.45
+/// Category: Normal
 
 
+double calculateBMI(double weight, double height){
+  return weight / (height * height);
+}
 
 
-Set<int> A = {1, 2, 3, 4, 5}, B = {4, 5, 6, 7, 8};
+String getBMICategory(double BMI){
 
+  if(BMI < 18.5) return 'Underweight';
+  else if(BMI < 25) return 'Normal';
+  else if (BMI < 30) return'Overweight';
+  else return 'Obese';
 
-void main(List<String> arguments) {
+}
 
-  print("Element of Set A:");
-  for(int x in A) {
-    print(x);
-  }
+void main(){
 
-  print("\nElement of Set B:");
-  for(int x in B){
-    print(x);
-  }
+  double weight = 70;
+  double height = 1.75;
+
+  double BMI = calculateBMI(weight, height);
+  print("Your BMI is: ${BMI.toStringAsFixed(2)}");
+
+  String category = getBMICategory(BMI);
+  print("Category: $category");
 
 }
