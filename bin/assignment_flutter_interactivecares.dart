eca4a00ad0f7a9012697d847a161a92f28638e23
Rajib Hasan <rajibhasan.mte@gmail.com>
2026-01-19 11:00:33 +0600

    almost completed

--- bin/assignment_flutter_interactivecares.dart
+++ bin/assignment_flutter_interactivecares.dart
@@ -1,14 +1,62 @@
-/// --------------------------------------------------------------------------------------------
+///  ------------------- Assignment 3: Opp dart --------------------
 /// Author: Rajib Hasan
-/// Submisson date:
+/// Submisson date: 18.01.2026
+/// Submisson deadline: 20.01.2026
 /// Submission to: interactivcecares flutter course
 /// submission links:
-///  ------------------- Assignment 3: Opp dart -----------------------
 
 
+
+class Person {
+
+  final String _name;
+  final int _age;
+
+  Person(this._name, this._age);
+
+  String get name => _name;
+  int get age => _age;
+
+  String getInfo() {
+    return 'Name: $_name, Age: $_age';
+  }
+}
+
+class Driver extends Person {
+  final String vehicle;
+
+  Driver(String name, int age, this.vehicle) : super(name, age);
+
+  @override
+  String getInfo() {
+    return 'Name: $name, Age: $age, Vehicle: $vehicle';
+  }
+}
+
+abstract class Ride {
+  double calculateFare(double distance);
+}
+
+class BikeRide extends Ride {
+  @override
+  double calculateFare(double distance) {
+    return distance * 10;
+  }
+}
+
+void printFare(Ride ride, double distance) {
+  double fare = ride.calculateFare(distance);
+  print('Ride Fare: $fare');
+}
 
 void main(){
 
-  
+  Driver driver = Driver('Rahim', 25, 'Bike');
+
+  print(driver.getInfo());
+
+  Ride bikeRide = BikeRide();
+
+  printFare(bikeRide, 5);
 
 }
